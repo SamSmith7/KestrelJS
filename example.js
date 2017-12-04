683f458cac1a3d29208e5d778061cce98953ab60
Sam Smith <samuel.smith@algomi.com>
2017-12-04 23:28:58 +0000

    wip - updates to build kestrel as an extended observable

--- example.js
+++ example.js
@@ -1,9 +1,38 @@
 const createServer = require('./index.js')
 
-const port = 3000
-const Server$ = createServer(port)
 
-Server$.do(({request, response}) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
-}).subscribe()
+const port = 3000
+const app = createServer(port)
+
+console.log(app.get('/').send)
+
+app.get('/').send()
+
+console.log(app)
+// Server$.do(({request, response}) => {
+//     console.log(request.url)
+//     response.end('Hello Node.js Server!')
+// }).subscribe()
+
+
+// app.get$('/', ({request, response}) => {
+//     console.log(request.url)
+//     response.end('Hello Node.js Server!')
+// })
+
+// const store = mongoDBStore
+// const passport = passport(config)
+// const errorHandler = err => {
+//
+//     return {
+//         code: 500,
+//         message: `Couldn't get User`
+//     }
+// }
+
+// app.get('/user')
+//     .verify(passport)
+//     .mergeMap(({body}) => store.fetch(body.userId))
+//     .map(UserVO)
+//     .catchAndSend(errorHandler)
+//     .send()
--- index.js
+++ index.js
@@ -1,18 +1,77 @@
 const http = require('http')
 const Rx = require('rxjs/Rx')
 
-const create = (port) => {
 
-    const handler$ = new Rx.Subject()
+class KestrelObservable extends Rx.Observable {
 
-    const server = http.createServer((request, response) => handler$.next({request, response}))
+    lift(operator) {
+        const observable = new KestrelObservable() //<-- important part here
+        observable.source = this
+        observable.operator = operator
+        observable.__internal = {
+            request: {},
+            response: {}
+        }
+        return observable
+    }
 
-    server.listen(port, err => {
-        err ? console.log('something bad happened', err) : console.log(`server is listening on ${port}`)
-        return
+    send() {
+
+        return SendSubscriber()
+    }
+}
+
+const SendSubscriber = () => {
+
+    const source = this
+
+    return source.subscribe(() => {
+
+        const { response } = source.__internal
+        response.end('Hello Node.js Server!')
     })
+}
 
-    return handler$
+const findMatch = urlToTest => incomingUrl => {
+
+    // Url Matcher
+    return true
+}
+
+const create = (port) => {
+    console.log(KestrelObservable)
+    const handler$ = KestrelObservable
+        .create(observer => {
+
+            const server = http.createServer((request, response) => observer.next({request, response}))
+
+            server.listen(port, err => {
+                err ? console.log('something bad happened', err) : console.log(`server is listening on ${port}`)
+                return
+            })
+        })
+
+    const urls = []
+
+    const urlMatch = (urlToMatch, method) => ({request}) => {
+
+        const incomingUrl = request.url
+        const firstMatch = _.findIndex(urls, findMatch(incomingUrl))
+
+        return method === request.method
+            && urlToMatch === urls[firstMatch]
+    }
+
+    const get = (url, options) => {
+
+        urls.push(url)
+        return handler$.filter(urlMatch(url, 'GET'))
+    }
+
+    return {
+
+        get
+    }
 }
 
 module.exports = create
